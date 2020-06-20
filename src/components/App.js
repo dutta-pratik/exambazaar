1b488575884a06dc1f549ae61f89e1fd862f6cb3
pratikdutta.786@gmail.com <pratikdutta.786@gmail.com>
2020-06-20 13:04:34 +0530

    initialized project

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import Stream from
+
+class App extends React.Component{
+
+  render(){
+    return (
+      <div className="App">
+        <Stream />
+      </div>
+    );
+  }
+}
+
+
+export default App;
+
